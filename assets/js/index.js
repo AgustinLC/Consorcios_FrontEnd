e91aae105d98da51735a22831a3302009ed72813
Arnaldo-Urizar <urizararnaldo7@gmail.com>
2023-10-17 17:36:36 -0300

    Optimización del código

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,19 @@
-window.onscroll = function () {
-    scroll = document.documentElement.scrollTop;
-    header = document.getElementById("header");
-    logoLight = document.querySelector(".logo-img.light");
-    logoDark = document.querySelector(".logo-img.dark");
+const header = document.getElementById("header");
+const logoLight = document.querySelector(".logo-img.light");
+const logoDark = document.querySelector(".logo-img.dark");
+const body = document.getElementById("container__all");
+const nav = document.getElementById("nav");
 
+const mostrar_menu = () =>{
+    body.classList.toggle('move_content');
+    header.classList.toggle('move_content');
+    nav.classList.toggle('move_nav');
+}
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+
+
+const headerFunction = () =>{
+    let scroll = document.documentElement.scrollTop;   
     if (scroll > 20) {
         header.classList.add('nav_mod');
         logoLight.style.display = 'none';
@@ -14,26 +24,16 @@ window.onscroll = function () {
         logoDark.style.display = 'none';
     }
 }
+window.onscroll = headerFunction;
 
-document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
 
-menu = document.getElementById("header");
-body = document.getElementById("container__all");
-nav = document.getElementById("nav");
-
-function mostrar_menu() {
-
-    body.classList.toggle('move_content');
-    menu.classList.toggle('move_content');
-    nav.classList.toggle('move_nav');
+const resizeWindow = ()=>{
+    window.addEventListener("resize", function () {
+        if (window.innerWidth > 760) {
+            body.classList.remove('move_content');
+            header.classList.remove('move_content');
+            nav.classList.remove('move_nav');
+        }
+    });
 }
 
-window.addEventListener("resize", function () {
-
-    if (window.innerWidth > 760) {
-        body.classList.remove('move_content');
-        menu.classList.remove('move_content');
-        nav.classList.remove('move_nav');
-    }
-
-});
